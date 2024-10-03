0eb40461b02cb67ed0fc722a6876da98652dfed7
PastushokTanya <127888657+PastushokTanya@users.noreply.github.com>
2024-10-03 17:54:02 +0300

    initial commit with GitHub actions and Gradle extensions

---
+++ src/main/java/com/tpastushok/cosmocats/CosmicCatsApplication.java
@@ -0,0 +1,13 @@
+package com.tpastushok.cosmocats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmicCatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmicCatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpastushok/cosmocats/CosmicCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpastushok.cosmocats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmicCatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
